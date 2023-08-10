27c15b7bf8d3942f9cb75be489f278c9ef50e08e
Elena <ririteaca@gmail.com>
2023-08-10 13:04:05 +0300

    drop down with errors

---
+++ src/app/components/Dropzone.js
@@ -0,0 +1,80 @@
+import React, { useMemo } from "react";
+// Import the useDropzone hooks from react-dropzone
+import { useDropzone } from "react-dropzone";
+
+
+const baseStyle = {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '20px',
+    borderWidth: 2,
+    borderRadius: 2,
+    borderColor: '#eeeeee',
+    borderStyle: 'dashed',
+    backgroundColor: '#fafafa',
+    color: '#bdbdbd',
+    outline: 'none',
+    transition: 'border .24s ease-in-out'
+  };
+  
+  const focusedStyle = {
+    borderColor: '#2196f3'
+  };
+  
+  const acceptStyle = {
+    borderColor: '#00e676'
+  };
+  
+  const rejectStyle = {
+    borderColor: '#ff1744'
+  };
+
+
+const Dropzone = ({ onDrop, accept }) => {
+  // Initializing useDropzone hooks with options
+  const { getRootProps, 
+          getInputProps, 
+          isFocused,
+          isDragAccept,
+          isDragReject,
+          isDragActive } = useDropzone({
+    onDrop,
+    accept
+  });
+
+  /* 
+    useDropzone hooks exposes two functions called getRootProps and getInputProps
+    and also exposes isDragActive boolean
+  */
+
+    const style = useMemo(() => ({
+        ...baseStyle,
+        ...(isFocused ? focusedStyle : {}),
+        ...(isDragAccept ? acceptStyle : {}),
+        ...(isDragReject ? rejectStyle : {})
+      }), [
+        isFocused,
+        isDragAccept,
+        isDragReject
+      ]);
+
+
+  return (
+    <div className="dropzone-div" {...getRootProps({style})}>
+      <input className="dropzone-input" {...getInputProps()} />
+      <div className="text-center">
+        {isDragActive ? (
+          <p className="dropzone-content">Release to drop the files here</p>
+        ) : (
+          <p className="dropzone-content">
+            Drag 'n' drop some files here, or click to select files
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Dropzone;
